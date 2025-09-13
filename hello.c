98d3984f845a652cdc380447f9ad5f242c7431aa
RohanCP26 <90731303+RohanCP26@users.noreply.github.com>
2025-09-13 22:43:10 +0000

    print name + email

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
-
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-  printf("Hello, World!\n");
-
-  return 0;
+int main(void){
+	const char *name ="Rohan";
+	const char *email = "paranjape.ro@northeastern.edu";
+	printf("%s\n",name);
+	printf("%s\n",email);
+	return 0;
 }
 
-/* vim: ts=4:sw=4 */
