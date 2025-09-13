4ddbf24aeb775f70b326f42d48d24b4c10ca28c6
RohanCP26 <90731303+RohanCP26@users.noreply.github.com>
2025-09-13 22:46:23 +0000

    repeat

--- hello.c
+++ hello.c
@@ -3,8 +3,10 @@
 int main(void){
 	const char *name ="Rohan";
 	const char *email = "paranjape.ro@northeastern.edu";
-	printf("%s\n",name);
-	printf("%s\n",email);
+	for(int i = 0; i < 20; i++){
+		printf("%s\n",name);
+		printf("%s\n",email);
+	}
 	return 0;
 }
 
